ed96fc5fcc1973f79beb50901092dacf4123c62b
Slava <SlavaShensev@gmail.com>
2021-01-20 11:40:23 +0200

    removeTask_id

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import './App.css';
 import {Todolist} from './Todolist';
 
 function App() {
-
-    const tasks = [
+    let tasks = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Rest API", isDone: false},
+        {id: 5, title: "GraphQL", isDone: false},
     ]
+
+    function removeTask(id: number) {
+        tasks = tasks.filter(t => t.id !== id)
+    }
 
     return (
         <div className="App">
-            <Todolist title="What to learn" tasks={tasks}/>
+            <Todolist title="What to learn" tasks={tasks}
+                      removeTask={removeTask}
+            />
         </div>
     );
 }
