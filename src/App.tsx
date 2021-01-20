331ec338001ebff248e399aa6f07c0ee56423d1f
Slava <SlavaShensev@gmail.com>
2021-01-20 11:54:59 +0200

    rerender_filterTasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 
 function App() {
-    let tasks = [
+    let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
         {id: 4, title: "Rest API", isDone: false},
         {id: 5, title: "GraphQL", isDone: false},
-    ]
+    ])
 
     function removeTask(id: number) {
-        tasks = tasks.filter(t => t.id !== id)
+        let filterTasks = tasks.filter(t => t.id !== id)
+        setTasks(filterTasks)
     }
 
     return (
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -9,6 +9,7 @@ type TaskType = {
 type PropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: (taskId: number) => void
 }
 
 export function Todolist(props: PropsType) {
@@ -20,12 +21,16 @@ export function Todolist(props: PropsType) {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/>
-                    <span>{props.tasks[0].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/>
-                    <span>{props.tasks[1].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/>
-                    <span>{props.tasks[2].title}</span></li>
+                {
+                    props.tasks.map(t => <li key={t.id}>
+                        <input type={'checkbox'} checked={t.isDone}/>
+                        <span>{t.title}</span>
+                        <button onClick={() => {
+                            props.removeTask(t.id)
+                        }}>x
+                        </button>
+                    </li>)
+                }
             </ul>
             <div>
                 <button>All</button>
